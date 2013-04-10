* Fix BC using numpy copy * Correct rho calculation no sum funtion, but f0+f1+... * Fix force calculation term F3 & F4 * Test script: forced fluid on periodic BC

lbm/lbm_kernel.py
@@ -38,11 +38,11 @@ def streamming(fd):
 def topbottomWallsBBNS_BC(fd):
     """Bounceback no-slip boundary conditions for top and bottom walls"""
     #up down, directions 2-4
-    fd[4][0],fd[2][-1] = fd[2][-1], fd[4][0]
+    fd[4][0],fd[2][-1] = np.copy(fd[2][-1]), np.copy(fd[4][0])
     #diagonal 5-7
-    fd[7][0],fd[5][-1] = np.roll( fd[5][-1],-1 ), np.roll(fd[7][0],1)
+    fd[7][0],fd[5][-1] = np.copy(np.roll( fd[5][-1],-1 )), np.copy(np.roll(fd[7][0],1))
     #diagonal 6-8
-    fd[8][0], fd[6][-1] = np.roll( fd[6][-1],1 ),  np.roll(fd[8][0],-1)
+    fd[8][0], fd[6][-1] =np.copy(np.roll( fd[6][-1],1 )),  np.copy(np.roll(fd[8][0],-1))
 
 def collision(ux,uy,rho,fd,tau):
     """Modify distribution funtions according to collision term"""
@@ -58,17 +58,16 @@ def init(LatticeSize,U,r):
     rho = r*np.ones(LatticeSize)
     return eqdistributions(ux,uy,rho)
 
-
 #Common operations
 def eqmacroVariables(fd,F=[0.,0.]):
     """Returns the macroscopic variables density and velocity.(rho, ux,uy)
        Equilibrium velocity includes forcing"""
-    rho = sum(fd)
+    rho = fd[0]+fd[1]+fd[2]+fd[3]+fd[4]+fd[5]+fd[6]+fd[7]+fd[8]
     ux  = (fd[1]-fd[3] + (fd[5]-fd[6]) + (fd[8]-fd[7]))/rho
     uy  = (fd[2]-fd[4] + (fd[5]+fd[6]) - (fd[8]+fd[7]))/rho
     #Additional forcing
-#    ux  += 0.5*F[0]/rho
-#    uy  += 0.5*F[1]/rho
+    ux  += 0.5*F[0]/rho
+    uy  += 0.5*F[1]/rho
     return ux,uy,rho
 
 def velocities(ux,uy):
@@ -76,23 +75,16 @@ def velocities(ux,uy):
     uy2 =uy**2
     u2  =ux2+uy2
     return ux2,uy2,u2
-    
-def preasuredrop(tau,endVel,rho,fd):
-    """Preasure drop as implemented by Succi"""
-    nu=(tau-0.5)/3.0
-    F =8*nu*endVel*rho/(6.0*len(rho)**2)
-    fd[[1,5,8]]+=F
-    fd[[3,6,7]]-=F
 
 def force(ux,uy,rho,fd,tau,F):
     """Adds microscopic forcing term"""
-    cf = (1-0.5*tau)*3.0
+    cf = (1-0.5/tau)*3.0
 
     F0 = 4.0/9.0  *(   -ux*F[0]  -    uy *F[1] )
     F1 = 1.0/9.0  *( (1-ux)*F[0] -    uy *F[1] + 3*ux*F[0] )
     F2 = 1.0/9.0  *(   -ux *F[0] + (1-uy)*F[1] + 3*uy*F[1] )
-    F3 = 1.0/9.0  *((-1-ux)*F[0] -    uy *F[1] - 3*ux*F[0] )
-    F4 = 1.0/9.0  *(   -ux *F[0] +(-1-uy)*F[1] - 3*uy*F[1] )
+    F3 = 1.0/9.0  *((-1-ux)*F[0] -    uy *F[1] + 3*ux*F[0] )
+    F4 = 1.0/9.0  *(   -ux *F[0] +(-1-uy)*F[1] + 3*uy*F[1] )
 
     F5 = 1.0/36.0 *( (1-ux)*F[0] + (1-uy)*F[1] + 3*( ux+uy)*( F[0]+F[1]) )
     F6 = 1.0/36.0 *((-1-ux)*F[0] + (1-uy)*F[1] + 3*(-ux+uy)*(-F[0]+F[1]) )

lbm/test.py
@@ -4,18 +4,18 @@ Test over 2D LBM
 """
 import lbm_kernel as lb
 import numpy as np
-
+from pylab import quiver,show,plot
 #intial conditions
-size=[10,20]
+size=[64,64]
 tau =1
 r=1
-
+F=[0.01,0]
 
 def conservationPBC():
     """Test if on periodic boundary conditions density
        and fluid velocity are conserved"""
     for i in range(2):
-        U = [-0.1,0.1*i]
+        U = [2,1*i]
         fd = lb.init(size,U,r)
         for step in range(200):
             ux,uy,rho = lb.eqmacroVariables(fd)
@@ -27,25 +27,63 @@ def conservationPBC():
         for i in range(len(magnitudes)):
             assert ( np.abs(magnitudes[i] - U[i]) < 1e-13 ).all()
         print 'Conservation of density and velocites on periodic BC: [OK]'
+        
+def conservationPBCforced():
+    """Test if on periodic boundary conditions density
+       and fluid velocity are conserved, when sistem is forced"""
+    for i in range(2):
+        U = [1,1*i]
+        fd = lb.init(size,U,r)
+        for step in range(200):
+            ux,uy,rho = lb.eqmacroVariables(fd,F)
+            lb.collision(ux,uy,rho,fd,tau)
+            lb.force(ux,uy,rho,fd,tau,F)
+            lb.streamming(fd)
+
+        ux,uy,rho = lb.eqmacroVariables(fd)
+        assert ( np.abs(rho - r) < 1e-13 ).all()
+        print 'Conservation of density and velocites on periodic BC and forced: [OK]'
 
 def conservationTBWalls():
     """ Test if on top & bottom walls density is conserved"""
-    for i in range(2):
-        U = [-0.1,0.1*i]
+    for i in range(1):
+        U = [0.1,0.1*i]
         fd = lb.init(size,U,r)
-        for step in range(5):
+        for step in range(2400):
             ux,uy,rho = lb.eqmacroVariables(fd)
             lb.collision(ux,uy,rho,fd,tau)
             lb.streamming(fd)
             lb.topbottomWallsBBNS_BC(fd)
         ux,uy,rho = lb.eqmacroVariables(fd)
-        assert ( np.abs(rho - r) < 1e-13 ).all()
+        assert ( np.abs(rho - r) < 1e-12 ).all()
+        plot(ux[:,10])
+        show()
+        
         print 'Conservation of density on periodic Flow and Upper and lower walls: [OK]'
+def conservationTBWallsforced():
+    """ Test if on top & bottom walls density is conserved"""
+    for i in range(1):
+        U = [0.1,0.1*i]
+        fd = lb.init(size,U,r)
+        for step in range(2800):
+            ux,uy,rho = lb.eqmacroVariables(fd,F)
+            lb.collision(ux,uy,rho,fd,tau)
+            lb.force(ux,uy,rho,fd,tau,F)
+            lb.streamming(fd)
+            lb.topbottomWallsBBNS_BC(fd)
+        ux,uy,rho = lb.eqmacroVariables(fd)
+        assert ( np.abs(rho - r) < 1e-12 ).all()
+        plot(ux[:,10])
+        show()
+        
+        print 'Conservation of density on forced periodic Flow and Upper and lower walls: [OK]'
 
 
 def main():
-    conservationPBC()
+#    conservationPBC()
+#    conservationPBCforced()
     conservationTBWalls()
+    conservationTBWallsforced()
         
 if __name__ == "__main__":
     main()