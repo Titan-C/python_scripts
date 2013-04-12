51ddcc4d0a4bbdf2887fd2a1ced2365003b20827
Óscar Nájera <najera.oscar@gmail.com>
2013-04-12 10:45:14 +0200

    Organize lbm_kernel code

--- lbm/lbm_kernel.py
+++ lbm/lbm_kernel.py
@@ -35,28 +35,6 @@ def streamming(fd):
     fd[7] = np.roll( np.roll(fd[7], 1,axis=0) ,-1,axis=1) #y v,x <
     fd[8] = np.roll( np.roll(fd[8], 1,axis=0) , 1,axis=1) #y v,x >
 
-def topbottomWallsBBNS_BC(fd):
-    """Bounceback no-slip boundary conditions for top and bottom walls"""
-    #up down, directions 2-4
-    fd[4,0],fd[2,-1]= np.copy(fd[2,-1]), np.copy(fd[4,0])
-    #diagonal 5-7
-    fd[7,0],fd[5,-1]= np.copy(np.roll( fd[5,-1],-1)), np.copy(np.roll(fd[7,0],1))
-    #diagonal 6-8
-    fd[8,0],fd[6,-1]= np.copy(np.roll( fd[6,-1],1 )), np.copy(np.roll(fd[8,0],-1))
-#    #no slip 1-3
-#    fd[3,0],fd[1,0] = np.copy(np.roll( fd[1,0],-1 )), np.copy(np.roll(fd[3,0],1))
-#    fd[3,-1],fd[1,-1]=np.copy(np.roll( fd[1,-1],-1 )),np.copy(np.roll(fd[3,-1],1))
-
-def lr_sideWallsBBNS_BC(fd):
-    """Bounceback no-slip boundary conditions for left - right walls"""
-    #up down, directions 2-4
-    fd[3,:,-1],fd[1,:,0] = np.copy(fd[1,:,0]), np.copy(fd[3,:,-1])
-    #diagonal 5-7
-    fd[7,:,-1],fd[5,:,0] = np.copy(np.roll( fd[5,:,0],1 )), np.copy(np.roll(fd[7,:,-1],-1))
-    #diagonal 6-8
-    fd[8,:,0], fd[6,:,-1] =np.copy(np.roll( fd[6,:,-1],1 )),  np.copy(np.roll(fd[8,:,0],-1))
-
-
 def collision(ux,uy,rho,fd,tau):
     """Modify distribution funtions according to collision term"""
     feq = eqdistributions(ux,uy,rho)
@@ -97,3 +75,23 @@ def force(ux,uy,rho,fd,tau,F):
     F8 = 1.0/36.0 *( (1-ux)*F[0] +(-1-uy)*F[1] + 3*( ux-uy)*( F[0]-F[1]) )
 
     fd+= cf*np.array([F0,F1,F2,F3,F4,F5,F6,F7,F8])
+
+##boundaries handling
+
+def topbottomWallsBBNS_BC(fd):
+    """Bounceback no-slip boundary conditions for top and bottom walls"""
+    #up down, directions 2-4
+    fd[4,0],fd[2,-1]= np.copy(fd[2,-1]), np.copy(fd[4,0])
+    #diagonal 5-7
+    fd[7,0],fd[5,-1]= np.copy(np.roll( fd[5,-1],-1)), np.copy(np.roll(fd[7,0],1))
+    #diagonal 6-8
+    fd[8,0],fd[6,-1]= np.copy(np.roll( fd[6,-1],1 )), np.copy(np.roll(fd[8,0],-1))
+
+def lr_sideWallsBBNS_BC(fd):
+    """Bounceback no-slip boundary conditions for left - right walls"""
+    #up down, directions 2-4
+    fd[3,:,-1],fd[1,:,0] = np.copy(fd[1,:,0]), np.copy(fd[3,:,-1])
+    #diagonal 5-7
+    fd[7,:,-1],fd[5,:,0] = np.copy(np.roll( fd[5,:,0],1 )), np.copy(np.roll(fd[7,:,-1],-1))
+    #diagonal 6-8
+    fd[8,:,0], fd[6,:,-1] =np.copy(np.roll( fd[6,:,-1],1 )),  np.copy(np.roll(fd[8,:,0],-1))
