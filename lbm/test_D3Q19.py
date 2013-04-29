3d454a260842c756d3605980a0491d0223de8bde
Óscar Nájera <najera.oscar@gmail.com>
2013-04-29 15:04:52 +0200

    Set lb_D3Q19 as default, using tensor ops rename test scripts Test pass poiseuille test on D2Q9 and D3Q19

--- lbm/test_D3Q19.py
+++ lbm/test_D3Q19.py
@@ -7,9 +7,9 @@ Created on Wed Apr 24 10:04:00 2013
 Tests for D3Q19 lattice
 """
 
-import lbtens_D3Q19
+import lb_D3Q19
 import numpy as np
-from classtest import ev_U, U_sol, anSolution, reynolds
+from test_D2Q9 import ev_U, U_sol, anSolution, reynolds
 
 #intial conditions
 size=[10,10,10]
@@ -20,7 +20,7 @@ F=np.array([1e-6,0,0])
 def test_consPBC():
     """Test if on periodic boundary conditions density and fluid velocity are conserved"""
     U=[0.1,0.1,0.1]
-    lat = lbtens_D3Q19.lattice(size,U,tau)
+    lat = lb_D3Q19.lattice(size,U,tau)
     for step in range(300):
        lat.updateMacroVariables()
        lat.collision()
@@ -33,7 +33,7 @@ def test_consPBC():
 
 def test_consPBCforced():
     """Test if on periodic boundary conditions density and fluid velocity are conserved, when sistem is forced"""
-    lat = lbtens_D3Q19.lattice(size,[0.,0.,0.],tau)
+    lat = lb_D3Q19.lattice(size,[0.,0.,0.],tau)
     for step in range(300):
        lat.updateMacroVariables(F)
        lat.collision()
@@ -45,7 +45,7 @@ def test_consPBCforced():
 def test_consArbiWalls():
     """ Test if with top & bottom walls density is conserved"""
     U=[0.1,0.0,0.]
-    lat = lbtens_D3Q19.lattice(size,U,tau)
+    lat = lb_D3Q19.lattice(size,U,tau)
     wall=np.zeros(size)
     wall[[0,-1]]=1
     lat.setWalls(wall)
@@ -60,25 +60,21 @@ def test_consArbiWalls():
 
 def test_poiseuille():
     """Test poiseuille Flow"""
-    size=[21,1,3]
-    lat = lbtens_D3Q19.lattice(size,[0.,0.,0.],tau)
-    uxold=np.ones(3).reshape(1,1,3)
-    step = 1
+    size=[21,1,1]
+    lat = lb_D3Q19.lattice(size,[0.,0.,0.],0.65)
     wall=np.zeros(size)
     wall[[0,-1]]=1
     lat.setWalls(wall)
-    while ( np.abs(uxold - lat.U[:,:,:,0]) > 1e-10 )[wall==0].all():
+    for i in range(18980):
        lat.collision()
        lat.force(F)
        lat.streamming()
        lat.onWallBBNS_BC(wall)
-       uxold=np.copy(lat.U[:,0,:,0])
        lat.updateMacroVariables(F)
-       step +=1
             
     an= anSolution(F[0],size[0],lat.nu)
     print lat.U[:,0,0,0]
     print an
-    print 'diff num - analytic: ', np.abs(lat.ux[:,0,0]-an)
+    print 'diff num - analytic: ', np.abs(lat.U[:,0,0,0]-an)
     
-    assert ( np.abs(lat.U[:,0,0,0]-an) < 3e-4 )[1:-1].all()
+    assert ( np.abs(lat.U[:,0,0,0]-an) < 2.25e-6 )[1:-1].all()
